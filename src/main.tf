b8a78265c3f32a29dae8f47f38bbee5c6afefdac
Marc-Philipp Esser <m-esser@mail.de>
2023-11-26 17:10:10 +0100

    remove terraform files

--- src/main.tf
+++
@@ -1,4 +0,0 @@
-resource "google_storage_bucket" "flights" {
-  name = format("%s-%s","flightrader24-flights",var.env)
-  location = var.gcp_default_region
-}
--- src/provider.tf
+++
@@ -1,7 +0,0 @@
-# GCP Provider
-
-provider "google" {
-  credentials = file(var.gcp_deployment_sa_key)
-  project = var.gcp_project_id
-  region = var.gcp_default_region
-}
