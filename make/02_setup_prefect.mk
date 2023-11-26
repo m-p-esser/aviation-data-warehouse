ecfffaabc5b6d06c94103680466898c9fb89dbe3
Marc-Philipp Esser <m-esser@mail.de>
2023-11-26 18:40:57 +0100

    update print statement for creating prefect blocks

--- make/02_setup_prefect.mk
+++ make/02_setup_prefect.mk
@@ -1,12 +1,14 @@
 ##@ [Prefect: Setup]
 
-.PHONY: setup-prefect:
-	@"$(MAKE)" create-prefect-blocks
+.PHONY: setup-prefect
+setup-prefect: ## Setup Prefect
+	@"$(MAKE)" update-prefect-blocks
 
-.PHONY: create-prefect-blocks
+
+.PHONY: update-prefect-blocks
 create-prefect-blocks: ## Create Prefect Blocks
 	@"$(MAKE)" env-init
-	@echo "Registering Prefect Blocks"
+	@echo "Registering/Updating Prefect Blocks"
 	prefect block register --file src/prefect/blocks/create_flightapi_api_key.py
 	prefect block register --file src/prefect/blocks/create_flightradar24_credentials.py
 	prefect block register --file src/prefect/blocks/create_gcp_credentials.py
