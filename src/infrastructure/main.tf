6018c0e337ff7736fdb019b52b887525f85da3f3
Marc-Philipp Esser <m-esser@mail.de>
2023-11-27 17:53:28 +0100

    fix format

--- src/infrastructure/main.tf
+++ src/infrastructure/main.tf
@@ -1,4 +1,4 @@
 resource "google_storage_bucket" "flights" {
-  name = format("%s-%s","flightrader24-flights",var.env)
+  name     = format("%s-%s", "flightrader24-flights", var.env)
   location = var.gcp_default_region
 }
