78d5990cb25c81a814dea6712359149c692dc261
Marc-Philipp Esser <m-esser@mail.de>
2023-11-28 11:39:34 +0100

    add bucket for flights data (Prod environment)

--- src/infrastructure/main.tf
+++ src/infrastructure/main.tf
@@ -7,3 +7,8 @@ resource "google_storage_bucket" "flights-test" {
   name     = format("%s-%s", "flightrader24-flights", "test")
   location = var.gcp_default_region
 }
+
+resource "google_storage_bucket" "flights-prod" {
+  name     = format("%s-%s", "flightrader24-flights", "prod")
+  location = var.gcp_default_region
+}
