8f55bc66cde826455a12a37432a23b7b0366f24e
Marc-Philipp Esser <m-esser@mail.de>
2023-11-28 10:41:03 +0100

    parametrize env_file_name input

--- src/utils.py
+++ src/utils.py
@@ -5,13 +5,10 @@ from dotenv import dotenv_values, find_dotenv
 
 def load_env_vars(env_file_name: str = ".env") -> dict:
     """Load environment variables from .env file and store as dict"""
-    base_env_file = find_dotenv(
+    env_file = find_dotenv(
         filename=env_file_name, raise_error_if_not_found=True, usecwd=True
     )
-    env_vars = dotenv_values(base_env_file)
-
-    if len(env_vars) > 0:
-        env_vars.update(env_vars)
+    env_vars = dotenv_values(env_file)
 
     return env_vars
 
@@ -25,13 +22,13 @@ def map_branch_to_env(branch: str):
     return env
 
 
-def update_env_based_on_git_branch(branch: str):
+def update_env_based_on_git_branch(branch: str, env_file_name: str):
     """Update Key Value pair in env file"""
 
     new_value = map_branch_to_env(branch)
 
     # Read the content of the existing .env file
-    with open(".env", "r") as file:
+    with open(env_file_name, "r") as file:
         lines = file.readlines()
 
     # Find and update the specified key
@@ -41,5 +38,5 @@ def update_env_based_on_git_branch(branch: str):
             break
 
     # Write the updated content back to the .env file
-    with open(".env", "w") as file:
+    with open(env_file_name, "w") as file:
         file.writelines(lines)
