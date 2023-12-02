55576fc761c890bb50ec1732f7e4397d69194d4a
Marc-Philipp Esser <m-esser@mail.de>
2023-12-02 12:21:15 +0100

    add missing fields to validation and fix data types checks for some fields

--- src/schema/raw.py
+++ src/schema/raw.py
@@ -14,16 +14,19 @@ class FlightsSchema(pa.DataFrameModel):
     ]  # = pa.Field(in_range={"min_value": -180, "max_value": 180})
     id: Series[pa.String] = pa.Field(unique=True)
     icao_24bit: Series[pa.String]
-    heading: Series[pa.String]
-    altitude: Series[pa.String]
-    ground_speed: Series[pa.String]
+    heading: Series[pa.Int]
+    altitude: Series[pa.Int]
+    ground_speed: Series[pa.Int]
     squawk: Series[pa.String]
     aircraft_code: Series[pa.String]
     registration: Series[pa.String]
-    time: Series[pa.String]
+    time: Series[pa.Int]
     origin_airport_iata: Series[pa.String]
     destination_airport_iata: Series[pa.String]
     number: Series[pa.String]
     airline_iata: Series[pa.String]
     on_ground: Series[pa.Int] = pa.Field(isin=[0, 1])
+    vertical_speed: Series[pa.Int]
+    callsign: Series[pa.String]
+    airline_icao: Series[pa.String]
     requested_at: Series[pa.DateTime]
